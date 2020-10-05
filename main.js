aac4725c03421a53bbd819b9413f148cf5bd07a4
j3ffh95 <j3ffh95@gmail.com>
2020-10-05 12:27:27 -0400

    Changed images

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const imgsContainer = document.getElementById("imgs");
 const imgs = document.querySelectorAll("#imgs img");
 // console.log(imgsContainer);
+
+let index = 0;
+
+const move = () => {
+  index++;
+
+  // if the index is more than how many images are in there then re assignit with 0
+  if (index > imgs.length - 1) index = 0;
+
+  //THis is the logic in order to move the picture and show the next one
+  imgsContainer.style.transform = `translateX(${-index * 500}px)`;
+};
+
+setInterval(move, 2000);
